7fcb25baf6fab66bf7c565e2ee1c581d4dff0644
Paulo Miguel <paulo.miguel@grupomateus.com.br>
2019-08-09 10:09:00 -0300

    feat: atualizar filial

--- frontend/src/components/Filiais.vue
+++ frontend/src/components/Filiais.vue
@@ -6,22 +6,16 @@
 
     <div class="add-filial">
       <div class="input">
-      <input v-model="filial.codigo" placeholder="Código" />
+        <input v-model="filial.codigo" placeholder="Código" />
       </div>
       <div class="input">
-      <input v-model="filial.cnpj" placeholder="CNPJ" />
+        <input v-model="filial.cnpj" placeholder="CNPJ" />
       </div>
       <div class="input">
-      <input v-model="filial.descricao" placeholder="Descrição" />
+        <input v-model="filial.descricao" placeholder="Descrição" />
       </div>
       <div class="input">
-      <input v-model="filial.empresa.id" placeholder="Empresa ID" />
-      </div>
-      <div class="input">
-      <input v-model="filial.empresa.codigo" placeholder="Empresa COD" />
-      </div>
-      <div class="input">
-      <input v-model="filial.empresa.descricao" placeholder="Empresa Descricao" />
+        <input v-model="filial.empresa.id" placeholder="Empresa ID" />
       </div>
       <h3>
         <a v-if="create" href="#" @click="addFilial">Adicionar Filial</a>
@@ -126,29 +120,44 @@ export default {
     async editFilial() {
       const res = await apolloClient.mutate({
         mutation: gql`
-          mutation updateEmpresa($id: ID!, $descricao: String!, $codigo: Int!, $ativo: Boolean!) {
-            updateEmpresa(id: $id, input: {
+          mutation updateFilial($id: ID!, $descricao: String!, $codigo: Int!, $cnpj: String!, $empresa: ID!, $ativo: Boolean!) {
+            updateFilial(id: $id, input: {
               descricao: $descricao
+              cnpj: $cnpj
               codigo: $codigo
+              empresa: [{
+                  id: $empresa 
+              }]
               ativo: $ativo
               }) {
               ok
-              empresa{
+              filial{
                 id
                 descricao
+                cnpj
                 codigo
                 ativo
+                empresa{
+                  id
+                  descricao
+                }
               }
             }
           }
         `,
         variables: {
-          id: this.empresa.id,
-          descricao: this.empresa.descricao,
-          codigo: this.empresa.codigo,
-          ativo: this.empresa.ativo
+          id: this.filial.id,
+          codigo: this.filial.codigo,
+          descricao: this.filial.descricao,
+          cnpj: this.filial.cnpj,
+          empresa: this.filial.empresa.id,
+          ativo: this.filial.ativo
         }
       })
+      const index = this.filiais.findIndex((element)=>{
+        return element.id == this.filial.id ? true : false
+      })
+      Object.assign(this.filiais[index], res.data.updateFilial.filial)
     },
     async addFilial() {
       const res = await apolloClient.mutate({
@@ -169,6 +178,7 @@ export default {
                         cnpj
                         descricao
                         empresa{
+                            id
                             descricao
                         }
                     }
@@ -183,12 +193,12 @@ export default {
         }
       })
       this.filiais.push(res.data.createFilial.filial) 
-      this.filiais.codigo = null
-      this.filiais.descricao = null
-      this.filiais.cnpj = null
-      this.filiais.empresa.id = null
-      this.filiais.empresa.codigo = null
-      this.filiais.empresa.descricao = null
+      this.filial.codigo = null
+      this.filial.descricao = null
+      this.filial.cnpj = null
+      this.filial.empresa.id = null
+      this.filial.empresa.codigo = null
+      this.filial.empresa.descricao = null
     }
   }
 }
